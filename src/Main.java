8d6392f325a3f2190c71fbaccdb049a444d1a7d6
Poomipat <poomipat.kh@easysunday.com>
2022-05-12 10:01:32 +0700

    Clean up println

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i]);
-        }
-
         Scanner sc= new Scanner(System.in); //System.in is a standard input stream
         System.out.print("Please enter a csv file path: ");
         String inputCsvPath = sc.nextLine();
@@ -16,6 +12,7 @@ public class Main {
         } catch (Exception e) {
             System.out.println("Error when writing output json file");
         }
+
     }
 }
 
